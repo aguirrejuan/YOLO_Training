eb90526ce67756994808fad84b803e38082af262
Juan Carlos Aguirre Arango <50622777+aguirrejuan@users.noreply.github.com>
2020-07-03 16:37:23 -0500

    Update train.py
    delete EarlyStopping(patience=3, verbose=1)

--- train.py
+++ train.py
@@ -173,11 +173,11 @@ def main(_argv):
 
         callbacks = [
             ReduceLROnPlateau(verbose=1),
-            EarlyStopping(patience=3, verbose=1),
+#            EarlyStopping(patience=3, verbose=1),
             ModelCheckpoint('checkpoints/yolov3_train_{epoch}.tf',
                             verbose=1,
                             save_weights_only=True,
-                            period=140),
+                            period=140), #1000
             TensorBoard(log_dir='logs')
         ]
 
