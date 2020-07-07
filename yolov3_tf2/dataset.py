aa1002b1a75605ccd46c540f77d5c54bee19f0b5
Juan Carlos Aguirre Arango <50622777+aguirrejuan@users.noreply.github.com>
2020-07-07 17:53:33 -0500

    Update dataset.py

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -120,9 +120,7 @@ def parse_tfrecord(tfrecord, class_table, size):
 
 def load_tfrecord_dataset(file_pattern, class_file, size=416):
     LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
-    class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(
-        class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
-
+    class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
     files = tf.data.Dataset.list_files(file_pattern)
     dataset = files.flat_map(tf.data.TFRecordDataset)
     return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
