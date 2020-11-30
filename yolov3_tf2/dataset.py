f24d3ba7dd1110cd92325f4e46a591cff928e00a
Juan Carlos Aguirre Arango <jucaguirrear@unal.edu.co>
2020-11-30 13:11:50 -0500

    Update dataset.py

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -103,9 +103,9 @@ def parse_tfrecord(tfrecord, class_table, size):
     x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (size, size))
 
-    class_text = tf.sparse.to_dense(
-        x['image/object/class/text'], default_value='')
-    labels = tf.cast(x['image/object/class/label'],tf.float32)#class_table.lookup(class_text), tf.float32)
+    class_text = tf.sparse.to_dense(x['image/object/class/label'])
+        #x['image/object/class/text'], default_value='')
+    labels = tf.cast(class_text, tf.float32)#tf.cast(class_table.lookup(class_text), tf.float32)
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
@@ -121,7 +121,7 @@ def parse_tfrecord(tfrecord, class_table, size):
 def load_tfrecord_dataset(file_pattern, class_file, size=416):
     LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
     files = tf.io.gfile.glob(file_pattern)#tf.data.Dataset.list_files(file_pattern)
-    class_table = None #tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
+    class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
     dataset = tf.data.TFRecordDataset(files)#files.flat_map(tf.data.TFRecordDataset)
     return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
 
